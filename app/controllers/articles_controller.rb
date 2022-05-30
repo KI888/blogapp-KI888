49afa3ed211828e508f58303cb6b0036958d4aa3
KI <keita687833@gmail.com>
2022-05-31 00:45:15 +0900

    記事詳細ページへのリンクを追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,12 @@ class ArticlesController < ApplicationController
     def index
         #render 'home/index'
         #@title = 'デイトラ'
-        @article = Article.first
+        @articles = Article.all
+    end
+
+
+    def show
+        @article = Article.find(params[:id])
     end
 
     #def about
