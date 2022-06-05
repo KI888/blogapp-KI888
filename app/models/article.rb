e857a3c0349d20b00193d87d161e904eb51c8bd5
KI <keita687833@gmail.com>
2022-06-05 13:55:34 +0900

    better errorsを導入

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 # $ rails generate model Article を実行すると作成される
 
 class Article < ApplicationRecord
@@ -5,11 +15,9 @@ class Article < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true
 
-
     # index.html.erb,show.html.erbで使用
     def display_created_at
         # selfにてarticleを取得
         I18n.l(self.created_at, format: :default)
     end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
   #rootは'/'を意味する ∴root to: は get '/' => 'home#index'と同じ
   #rootを使用することでURLヘルパー指定が可能になる rails/info/routesにヘルパーurlが追加される
   #root to: 'home#index'
